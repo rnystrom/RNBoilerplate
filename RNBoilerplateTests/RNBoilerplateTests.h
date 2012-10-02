c92584242223a2893030246c0008d120326b7025
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-10-02 19:58:48 -0400

    Initial Commit

---
+++ RNBoilerplateTests/RNBoilerplateTests.h
@@ -0,0 +1,13 @@
+//
+//  RNBoilerplateTests.h
+//  RNBoilerplateTests
+//
+//  Created by Ryan Nystrom on 10/2/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RNBoilerplateTests : SenTestCase
+
+@end
---
+++ RNBoilerplateTests/RNBoilerplateTests.m
@@ -0,0 +1,32 @@
+//
+//  RNBoilerplateTests.m
+//  RNBoilerplateTests
+//
+//  Created by Ryan Nystrom on 10/2/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
+//
+
+#import "RNBoilerplateTests.h"
+
+@implementation RNBoilerplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RNBoilerplateTests");
+}
+
+@end
